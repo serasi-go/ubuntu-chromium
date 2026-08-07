f2c204a34759f9e605ed03e8c75375eb1e99675e
garudakencana <130397271+serasi-go@users.noreply.github.com>
2026-08-07 16:51:57 +0700

    Delete jupyter_server_config.py

--- jupyter_server_config.py
+++
@@ -1,7 +0,0 @@
-# Memaksa agar server proxy Binder mengizinkan jembatan WebSocket VNC tetap menyala
-c.ServerApp.allow_remote_xul = True
-c.ServerApp.tornado_settings = {
-    'headers': {
-        'Content-Security-Policy': "frame-ancestors 'self' https://*.mybinder.org https://*.gesis.org;"
-    }
-}
